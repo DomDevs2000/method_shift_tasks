fe56abea467b99f9f00480a7f98a41019f71850b
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-10 00:34:53 +0000

    updated test (model name update)

--- tasks/tests/test_cycle_time.py
+++ tasks/tests/test_cycle_time.py
@@ -2,13 +2,13 @@ import datetime
 
 import pytest
 
-from tasks.models import Tasks
+from tasks.models import Task
 
 
 @pytest.mark.django_db
 def test_cycle_time():
     start_date = datetime.datetime(2022, 1, 1, 12, 0, 0)
     end_date = datetime.datetime(2022, 1, 3, 14, 30, 0)
-    task: Tasks = Tasks(task_name="Test Task", start_date=start_date, end_date=end_date)
+    task: Task = Task(task_name="Test Task", start_date=start_date, end_date=end_date)
 
     assert task.cycle_time == "2 days, 2 hours"
