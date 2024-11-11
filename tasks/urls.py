81ba33d17893f83fdb5e05617cdc3bd6c75eb1c5
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-11 18:17:36 +0000

    updated url

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.get_all_tasks, name="get_all_tasks"),
     path("create/", views.create_task, name="create_task"),
     path("<int:pk>/", views.get_task_by_id, name="task_by_id"),
-    path('metrics/display-average-cycle-time/', display_average_cycle_time, name='display-average-cycle-time'),
+    path('metrics/report/', display_average_cycle_time, name='display-average-cycle-time'),
 ]
