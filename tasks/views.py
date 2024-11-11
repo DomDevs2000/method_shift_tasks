147de5f9e50a83a88606a0385de5b262aa6312d6
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-11-11 18:12:18 +0000

    updated view return types

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,14 @@
 # Create your views here.
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpRequest
-from django.shortcuts import redirect, render
+from django.shortcuts import HttpResponse, redirect, render
 
 from .forms import TaskForm
 from .models import Task, Metric
 from .tasks import calculate_average_cycle_time
 
 
-def create_task(request: HttpRequest):
+def create_task(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form: TaskForm = TaskForm(request.POST)
         if form.is_valid():
@@ -20,7 +20,7 @@ def create_task(request: HttpRequest):
     return render(request, "create_task.html", {"form": form})
 
 
-def get_all_tasks(request: HttpRequest):
+def get_all_tasks(request: HttpRequest)-> HttpResponse:
     tasks: Task = Task.objects.all()
     paginator: Paginator = Paginator(tasks, 15)
     page = request.GET.get('page')
@@ -34,7 +34,7 @@ def get_all_tasks(request: HttpRequest):
     return render(request, 'all_tasks.html', {'tasks': tasks})
 
 
-def get_task_by_id(request: HttpRequest, pk: int):
+def get_task_by_id(request: HttpRequest, pk: int)-> HttpResponse:
     try:
         task: Task = Task.objects.get(pk=pk)
 
@@ -44,12 +44,12 @@ def get_task_by_id(request: HttpRequest, pk: int):
         return render(request, "404.html", {"error": "No task found"}, status=404)
 
 
-def display_average_cycle_time(request):
+def display_average_cycle_time(request)-> HttpResponse:
     tasks = Task.objects.all()
     average_cycle_time: float = Task.average_cycle_time(tasks)
     metric = Metric.update_average_cycle_time(average_cycle_time)
     return render(request, 'display_average_cycle_time.html', {'metric': metric})
 
 
-def error_404_view(request: HttpRequest, exception):
+def error_404_view(request: HttpRequest, exception)-> HttpResponse:
     return render(request, "404.html", status=404)
